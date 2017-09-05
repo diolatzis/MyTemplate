4c9fba0a913206aabc768c95c539f4e42aeaf8e7
stavros diolatzis <diolatzis@gmail.com>
2017-09-05 16:55:31 +0300

    Splash screen implemented the right way with no delay

--- app/src/main/java/com/diol/apps/mytemplate/view/activity/SplashActivity.java
+++ app/src/main/java/com/diol/apps/mytemplate/view/activity/SplashActivity.java
@@ -7,22 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import com.diol.apps.mytemplate.R;
 import com.diol.apps.mytemplate.utility.Utils;
 
-/**
- * Created by diola on 23/8/2017.
- */
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Utils.runAfter(getResources().getInteger(R.integer.splash_duration), new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
